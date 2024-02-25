283f6eb1f3e5a7de51c8e325fa0b794ab81e90a7
devadethanr <devadethan1122@gmail.com>
2024-02-25 20:54:15 +0530

    get, post methods

--- flasknew/app.py
+++ flasknew/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ["GET","POST"])
 def index():
     # if "name" in request.args:
     #     name = request.args["name"]
@@ -10,9 +10,15 @@ def index():
     # #     name = "World"
     # name = request.args.get("name", "World")
     # return render_template("index.html", name = name)
+    if request.method == "POST":
+        name = request.form.get("name", "world")
+        return render_template("/greet.html", name = name)
     return render_template("index.html")
 
-@app.route("/greet")
-def greet():
-    name = request.args.get("name", "world")
-    return render_template("/greet.html", name = name)
+
+# @app.route("/greet", methods = ["GET","POST"])
+# def greet():
+#     # name = request.args.get("name", "world") .ARGS ARE FOR GET REQUESTS
+#     #.FORMS ARE FOR POST REQUESTS
+#     name = request.form.get("name", "world")
+#     return render_template("/greet.html", name = name)
